244f634073a04fc6b043881b901603dd081fc3cd
Sergei Chestakov <sergei332@gmail.com>
2017-07-08 17:17:52 -0700

    Removed unused dependencies

---
+++ data/scrapeData.py
@@ -0,0 +1,67 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+page = requests.get("http://104.236.158.81/africa/dataall/1.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+countries = ['Angola',
+'Benin',
+'Botswana',
+'Burkina Faso',
+'Burundi',
+'Cameroon',
+'Cape Verde',
+'Central African Republic',
+'Chad',
+'Comoros',
+'Congo, Dem. Rep.',
+'Congo, Rep.',
+'Cte dIvoire',
+'Djibouti',
+'Equatorial Guinea',
+'Eritrea',
+'Ethiopia',
+'Gabon',
+'Gambia, The',
+'Ghana',
+'Guinea',
+'Guinea-Bissau',
+'Kenya',
+'Lesotho',
+'Liberia',
+'Madagascar',
+'Malawi',
+'Mali',
+'Mauritania',
+'Mauritius',
+'Mozambique',
+'Namibia',
+'Niger',
+'Nigeria',
+'Rwanda',
+'So Tom and Prncipe',
+'Senegal',
+'Seychelles',
+'Sierra Leone',
+'Somalia',
+'South Africa',
+'Sudan',
+'Swaziland',
+'Tanzania',
+'Togo',
+'Uganda',
+'Zambia',
+'Zimbabwe',
+'NORTH AFRICA',
+'Algeria',
+'Egypt, Arab Rep.',
+'Libya',
+'Morocco',
+'Tunisia']
+
+elements = soup.findAll("div", { "class" : "t" })
+for a in elements:
+    text = a.getText().encode('ascii', 'ignore').decode('ascii')
+    if any(c.isalpha() for c in text) and text in countries:
+        print("Data: " + elements[elements.index(a) + 3].getText() + "     " + text)
--- js/interact.js
+++ js/interact.js
@@ -2,62 +2,62 @@ var map = AmCharts.makeChart("chartdiv", {
   "type": "map",
   "theme": "light",
   "dataProvider": {
-  "map": "worldLow",
-  "zoomLevel": 2.7,
-  "zoomLongitude": 24.0174,
-  "zoomLatitude": -1.1076,
-  "areas": [
-    { "id": "AO" },
-    { "id": "BF" },
-    { "id": "BI" },
-    { "id": "BJ" },
-    { "id": "BW" },
-    { "id": "CD" },
-    { "id": "CF" },
-    { "id": "CG" },
-    { "id": "CI" },
-    { "id": "CM" },
-    { "id": "DJ" },
-    { "id": "DZ" },
-    { "id": "EG" },
-    { "id": "ER" },
-    { "id": "ET" },
-    { "id": "GA" },
-    { "id": "GH" },
-    { "id": "GM" },
-    { "id": "GN" },
-    { "id": "GQ" },
-    { "id": "GW" },
-    { "id": "KE" },
-    { "id": "LR" },
-    { "id": "LS" },
-    { "id": "LY" },
-    { "id": "MA" },
-    { "id": "MG" },
-    { "id": "ML" },
-    { "id": "MR" },
-    { "id": "MW" },
-    { "id": "MZ" },
-    { "id": "NA" },
-    { "id": "NE" },
-    { "id": "NG" },
-    { "id": "RW" },
-    { "id": "SD" },
-    { "id": "SL" },
-    { "id": "SN" },
-    { "id": "SO" },
-    { "id": "SS" },
-    { "id": "SZ" },
-    { "id": "TD" },
-    { "id": "TG" },
-    { "id": "TN" },
-    { "id": "TZ" },
-    { "id": "UG" },
-    { "id": "ZA" },
-    { "id": "ZM" },
-    { "id": "ZW" },
-    { "id": "EH" }
-  ]
+    "map": "worldLow",
+    "zoomLevel": 2.7,
+    "zoomLongitude": 24.0174,
+    "zoomLatitude": -1.1076,
+    "areas": [
+      { "id": "AO",},
+      { "id": "BF" },
+      { "id": "BI" },
+      { "id": "BJ" },
+      { "id": "BW" },
+      { "id": "CD" },
+      { "id": "CF" },
+      { "id": "CG" },
+      { "id": "CI" },
+      { "id": "CM" },
+      { "id": "DJ" },
+      { "id": "DZ" },
+      { "id": "EG" },
+      { "id": "ER" },
+      { "id": "ET" },
+      { "id": "GA" },
+      { "id": "GH" },
+      { "id": "GM" },
+      { "id": "GN" },
+      { "id": "GQ" },
+      { "id": "GW" },
+      { "id": "KE" },
+      { "id": "LR" },
+      { "id": "LS" },
+      { "id": "LY" },
+      { "id": "MA" },
+      { "id": "MG" },
+      { "id": "ML" },
+      { "id": "MR" },
+      { "id": "MW" },
+      { "id": "MZ" },
+      { "id": "NA" },
+      { "id": "NE" },
+      { "id": "NG" },
+      { "id": "RW" },
+      { "id": "SD" },
+      { "id": "SL" },
+      { "id": "SN" },
+      { "id": "SO" },
+      { "id": "SS" },
+      { "id": "SZ" },
+      { "id": "TD" },
+      { "id": "TG" },
+      { "id": "TN" },
+      { "id": "TZ" },
+      { "id": "UG" },
+      { "id": "ZA" },
+      { "id": "ZM" },
+      { "id": "ZW"},
+      { "id": "EH" }
+    ]
   },
   "areasSettings": {
     "autoZoom": true,
@@ -81,8 +81,8 @@ var map = AmCharts.makeChart("chartdiv", {
 function clicked(event){
   var country = event.mapObject.title;
   document.getElementById("countryName").innerHTML = country;
-  $("#title").slideUp("slow");
   document.getElementById("myNav").style.height = "100%";
+  $("#title").slideUp("slow");
 }
 
 /* Close */
@@ -91,15 +91,4 @@ function zoomOut() {
     map.selectObject(map.dataProvider);
     map.fire({type: "homeButtonClicked", chart: map});
     $("#title").slideDown("slow");
-}
-
-var btnRadius = $('.cd-modal-bg').width()/2,
-	left = $('.cd-modal-bg').offset().left + btnRadius,
-	top = $('.cd-modal-bg').offset().top + btnRadius - $(window).scrollTop(),
-	scale = scaleValue(top, left, btnRadius, $(window).height(), $(window).width());
-
-function scaleValue( topValue, leftValue, radiusValue, windowW, windowH) {
-	var maxDistHor = ( leftValue > windowW/2) ? leftValue : (windowW - leftValue),
-		maxDistVert = ( topValue > windowH/2) ? topValue : (windowH - topValue);
-	return Math.ceil(Math.sqrt( Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2) )/radiusValue);
 }
