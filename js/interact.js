9efee159856d2eeda1763ceb9191273cc66698e3
miguel-acevedo <acevedomiguel@outlook.com>
2017-07-09 13:15:19 -0700

    Loads json in cross-origin environment

--- js/interact.js
+++ js/interact.js
@@ -101,6 +101,14 @@ var data = [{
     "gdp": "4"
 }]
 
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
 var dataLoad = null;
 
 function loadGDP(name) {
